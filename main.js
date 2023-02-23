27883df7617ec15d046da60c270db852d662243f
pranabcode2023 <pranabcode2023@gmail.com>
2023-02-23 15:07:51 +0100

    2nd commit

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log(seaFood.meals);
+
+
+
+  document.getElementById('searchfield').addEventListener('keyup', function(){
+    let pattern = new RegExp(this.value, 'i');
+    let resultSet = seaFood.meals.filter(item => item.strMeal.match(pattern) && this.value != '').map(item => `<a href="${item.seaFood}">${item.strMeal} ${item.strMealThumb} ${item.idMeal}</a>`).join(', ');
+    document.getElementById('searchresult').innerHTML = resultSet;
+  });
+
+
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
