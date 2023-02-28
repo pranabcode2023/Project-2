ea9a03187eca134ef45318b9e2ea4a8c96871a5e
pranabcode2023 <pranabcode2023@gmail.com>
2023-02-28 10:26:34 +0100

    some changes

--- main.js
+++ main.js
@@ -1,26 +1,119 @@
-console.log(seaFood.meals);
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const mealEl = document.getElementById("meals");
+const resultHeading = document.getElementsByClassName("result-heading");
+const single_mealEl = document.getElementById("single-meal");
+
+// search recipe
+
+function searchMeal(e){
+    e.preventDefault();
 
 
-
-  document.getElementById('searchfield').addEventListener('keyup', function(){
-    let pattern = new RegExp(this.value, 'i');
-    let resultSet = seaFood.meals.filter(item => item.strMeal.match(pattern) && this.value != '').map(item => `<a href="${item.seaFood}">${item.strMeal} ${item.strMealThumb} ${item.idMeal}</a>`).join(', ');
-    document.getElementById('searchresult').innerHTML = resultSet;
-  });
+    // clear single-meal
+    single_mealEl.innerHTML="";
 
 
-  function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
+    // search recipe
+
+    const term = search.value;
+
+    // check for empty
+
+    if (term.trim()){
+       fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+       .then(res => res.json())
+       .then(data => {
+            console.log(data);
+            resultHeading.innerHTML =`<h2> Search Results for ${term}`;
+            if (data.meals === null){
+                resultHeading.innerHTML=`<h2>No results found for ${term}`;
+            }else{
+                mealEl.innerHTML= data.meals.map(
+                    meal => `
+                    <div class="meal">
+                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                    <div class="meal-info" data-mealID="${meal.idMeal}">
+                    <h3>${meal.strMeal}</h3>
+                    <div/>
+                    </div>
+                    `
+                )
+                .join("");
+            }
+       });
+       }else{
+        alert("Please enter a recipe name");
+       }
+
     }
-  }
+
+// fetch meal by id
+ function  getMealById(mealID) {
+    fetch 
+    (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+       const meal =data.meals[0];
+       addMealToDOM (meal);
+    });
+ }
+
+
+
+//  add meal to DOM
+
+function addMealToDOM(meal){
+    const ingredients = [];
+    for (let i=0 ; i<=20; i++){
+        if (meal[`strIngredient${i}`]){
+            ingredients.push(`${meal[`strIngredient${i}`]}
+            -
+            ${meal[`strMeasure${i}`]}
+            
+            `);
+        }else{
+            break;
+        }
+    }
+    single_mealEl.innerHTML = `
+    <div class="single-meal">
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+        <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>`:""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>`:""}
+    <div/>
+    <div class = "main">
+    <p>${meal.setInstructions}</p>
+    <h2>Ingredients</h2>
+    <ul>
+    ${ingredients.map(ing => `<li>${ing}</li>`).join("")}
+    </ul>
+    </div>
+    </div>
+    `
+}
+
+
+
+// EventListener
+
+submit.addEventListener("submit", searchMeal);
+mealEl.addEventListener("click",e =>{
+    const mealInfo = e.path.find(item => {
+        if (item.classlist){
+            return item.classlist.contains("meal-info");
+        }else{
+            return false;
+        }
+    });
+
+    if (mealInfo){
+        const mealID = mealInfo.getAttribute("data-mealid");
+        getMealById(mealID);
+    }
+
+});
+
