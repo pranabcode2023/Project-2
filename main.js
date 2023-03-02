e6f8bcd2a20ed216dca7f82488c0c652081bb9cd
pranabcode2023 <pranabcode2023@gmail.com>
2023-03-02 13:05:03 +0100

    some changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// intial reference
+// Search for Recipe
 
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
@@ -87,9 +87,64 @@ searchBtn.addEventListener("click",()=>{
 
 
 
+// image gallery
+
+
+const image_gallery = document.querySelector(".image--container")
+const image_container = document.querySelector(".image--selection")
+const loading = '<h1 class="loader">Loading...</h1>'
 
 
 
+
+const showImages = () => {
+    // Show loading text if no data
+    if (image_container.children.length === 0) image_container.innerHTML = loading
+    fetch(`https://api.unsplash.com/photos?client_id=${apiKey}`)
+      .then(res => {
+        res.json().then(images => {
+          // Call the function to create the gallery
+          createImageGallery(images)
+        })
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+
+  const createImageGallery = images => {
+    let output = ""
+    // Show the first image on the viewer
+    image_gallery.innerHTML = `<img src="${images[0].urls.regular}" class="animate-entrance image--gallery" alt="${images[0].alt_description}">`
+    setTimeout(() => {
+      image_gallery.children[0].classList.remove("animate-entrance")
+    }, 500)
+    // show unselected images
+    images.forEach(({ urls, alt_description }) => {
+      output += `<img src="${urls.regular}" alt="${alt_description}" class="image__item" />`
+    })
+    image_container.innerHTML = output
+  }
+
+
+
+  const changeImage = e => {
+    // Get the viewer image element
+    const image = image_gallery.children[0]
+    if (e.target.src) {
+      // change the image
+      image.classList.add("animate-entrance")
+      image.src = e.target.src
+      setTimeout(() => {
+        image.classList.remove("animate-entrance")
+      }, 800)
+    }
+  }
+  
+  // Event listeners
+  document.addEventListener("DOMContentLoaded", showImages)
+  image_container.addEventListener("click", changeImage)
 
 
 
